705a85d71ce2fb92602b3aece9b2a571917b933a
Kai Wißler <kai.wissler@schwarzfisch.virtual-identity.net>
2019-10-24 15:06:19 +0200

    feat: Adjust normalizer

--- src/cms-adapter/prismic/helpers/normalize-prismic-data.ts
+++ src/cms-adapter/prismic/helpers/normalize-prismic-data.ts
@@ -21,50 +21,37 @@ export const normalizePrismicData = async (rawContentData: PrismicData, api: Res
             const normalizedSubField = [];
 
             // ...iterate over array entries
-            for(const subField of contentField) {
+            for(const contentObject of contentField) {
 
-                /*// if entry is array...
-                if(Array.isArray(subField)) {
-                    console.log(subField);
-                    
+                // if is a seperate content to be fetched
+                if(contentObject.type && contentObject.id) {
+                    const subContentData = alreadyNormalizedContents[contentObject.id] || await api.getByID(contentObject.id).then(res => normalizePrismicData((res as any), api, alreadyNormalizedContents));
+                    normalizedSubField.push({type: contentObject.type, data: subContentData.data});
+                }
 
-                    // ...iterate over array entries
-                    for(const subSubField of subField) {
-                        const subContent = subSubField;
-                        if(subContent.id) {
-                            const subContentData = alreadyNormalizedContents[subContent.id] || await api.getByID(subContent.id).then(res => normalizePrismicData((res as any), api, alreadyNormalizedContents));
-                            normalizedSubField.push({type: subContent.type, data: subContentData.data});
-                        }
-                        else {
-                            normalizedSubField.push(subField);
-                        }
-                    }
-                } else {*/
-                    if(subField.type && subField.id) {
-                        const subContentData = alreadyNormalizedContents[subField.id] || await api.getByID(subField.id).then(res => normalizePrismicData((res as any), api, alreadyNormalizedContents));
-                        normalizedSubField.push({type: subField.type, data: subContentData.data});
-                    }
-                    else if(subField[Object.keys(subField)[0]]) {
-                        const subContent = subField[Object.keys(subField)[0]];
+                // if has one subobject (prismic specific 'group')
+                else if(contentObject[Object.keys(contentObject)[0]]) {
+                    const subContent = contentObject[Object.keys(contentObject)[0]];
 
-                        if(subContent.id) {
-                            const subContentData = alreadyNormalizedContents[subContent.id] || await api.getByID(subContent.id).then(res => normalizePrismicData((res as any), api, alreadyNormalizedContents));
-                            normalizedSubField.push({type: subContent.type, data: subContentData.data});
-                        }
-                        else {
-                            normalizedSubField.push(subField);
-                        }
+                    if(subContent.id) {
+                        const subContentData = alreadyNormalizedContents[subContent.id] || await api.getByID(subContent.id).then(res => normalizePrismicData((res as any), api, alreadyNormalizedContents));
+                        normalizedSubField.push({type: subContent.type, data: subContentData.data});
                     }
-               /* }*/
+                    else {
+                        normalizedSubField.push(contentObject);
+                    }
+                }
             }
             normalizedContent.data[fieldIdentifier] = normalizedSubField;
             alreadyNormalizedContents[rawContentData.id] = normalizedContent;
         }
         else {
+            // if is a seperate content to be fetched
             if(contentField.id && contentField.type) {
                 const contentFieldData = alreadyNormalizedContents[contentField.id] || await api.getByID(contentField.id).then(res => normalizePrismicData((res as any), api, alreadyNormalizedContents));
                 normalizedContent.data[fieldIdentifier] = {type: contentField.type, data: contentFieldData.data};
             }
+            // if is 'pure' data
             else {
                 normalizedContent.data[fieldIdentifier] = contentField;
             }
