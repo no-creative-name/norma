89684d33314ee46848e6cb1c7efa2f7edaca0cb7
Kai Wißler <kai.wissler@schwarzfisch.fritz.box>
2019-11-03 13:24:58 +0100

    feat: Adjust main

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,20 @@ import { adapterConfig } from "../adapter.config";
 import { getCmsAdapter } from "./cms-adapter/get-cms-adapter";
 import { ContentAdapter } from "./content-adapter";
 
-window.onload = async () => {
-    const contentAdapter = new ContentAdapter(adapterConfig);
+let contentAdapter: ContentAdapter;
+
+export const getContentAdapter = (): ContentAdapter => {
+    if (!contentAdapter) {
+        contentAdapter = new ContentAdapter(adapterConfig);
+    }
+    return contentAdapter;
+};
+
+module.exports = getContentAdapter;
+
+/*window.onload = async () => {
+    contentAdapter = new ContentAdapter(adapterConfig);
 
     // tslint:disable-next-line
     console.log(await contentAdapter.getContent("44eh1S0IXmC0LeMciK6z6t", "de"));
-};
+};*/
