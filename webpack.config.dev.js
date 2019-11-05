65e3af9b77d39b657ed9b002a0b83a4d728b59a9
Kai Wißler <kai.wissler@schwarzfisch.fritz.box>
2019-11-05 13:27:04 +0100

    feat: Add two different webpack configs

---
+++ webpack.config.dev.js
@@ -0,0 +1,31 @@
+const path = require("path");
+const DtsBundleWebpack = require("dts-bundle-webpack");
+
+module.exports = {
+    devServer: {
+        contentBase: "./",
+        compress: true,
+        hot: true,
+        port: 3000,
+        publicPath: "/",
+    },
+    entry: path.join(__dirname, "/src/main.ts"),
+    output: {
+        filename: "dist/index.js",
+        path: __dirname,
+        library: "norma",
+        libraryTarget: "umd",
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                loader: "ts-loader",
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    resolve: {
+        extensions: [".tsx", ".ts", ".js"],
+    }
+};
