e87f9bcb684446ea9106fef07e444571d0504e7d
Kai Wißler <kai.wissler@schwarzfisch.fritz.box>
2019-11-10 17:35:52 +0100

    feat: Lint & build

--- src/handle-content/content-handler.ts
+++ src/handle-content/content-handler.ts
@@ -1,4 +1,8 @@
 import { IContentConfig, IFieldConfig } from "../interfaces/adapter-config";
 import { IContent } from "../interfaces/content";
 
-export type ContentHandler = (content: IContent, contentConfigs?: IContentConfig[], fieldConfigs?: IFieldConfig[]) => IContent;
+export type ContentHandler = (
+    content: IContent,
+    contentConfigs?: IContentConfig[],
+    fieldConfigs?: IFieldConfig[],
+) => IContent;
